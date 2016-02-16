fcadcac4c47e69ed7283ceff54d566c68ecefccb
Patrick Liesveld <{First}{Last}.a.t.{mail google}>
2016-02-16 11:25:37 -0500

    Moved invovation of espeak from os.system to subprocess.call Added StringVar to contain application executable filename Added dialog box to select espeak application location fixed imports for python3

--- gui.py
+++ gui.py
@@ -1,8 +1,24 @@
-from Tkinter import *
 import string
 import json
-import os
-import pyhk
+from subprocess import call
+import sys
+import traceback
+
+try: # python2
+    from Tkinter import *
+    from tkFileDialog import *
+    import tkMessageBox
+except ImportError: #python3
+    from tkinter import *
+    from filedialog import *
+
+HOTKEYS_ENABLED=False
+
+try:
+    import pyhk
+    HOTKEYS_ENABLED=true
+except ImportError:
+    print("Could not load hotkeys")
 
 
 """
@@ -17,19 +33,21 @@ class MuteSpeak(Frame):
     def __init__(self, master=None,**kwargs):
         Frame.__init__(self, master)
         Pack.config(self)
+        self.parent = master
 
         self.createVars()
         self.reset(**kwargs)
         self.createFileMenu()
         self.createWidgets()
+        self.registerEvents()
 
     def createVars(self):
+        self.app = StringVar()
         self.amplitude = IntVar()
         self.pitch = IntVar()
         self.wpm = IntVar()
         self.gap = IntVar()
         self.clearOnMessage = BooleanVar()
-        self.hot = pyhk.pyhk()
 
     def reset_defaults(self):
         self.amplitude.set(100)
@@ -137,13 +155,47 @@ class MuteSpeak(Frame):
         self.message.set("Hello")
         self.entry_message.config(textvariable=self.message)
 
-        self.hot.addHotkey(['Return'], self.speak_contents, isThread=True)
-        #self.entry_message.bind('<Key-Return>', self.speak_contents)
+
+    def registerEvents(self):
+        if HOTKEYS_ENABLED:
+           self.hot = pyhk.pyhk()
+           self.hot.addHotkey(['Return'], self.speak_contents, isThread=True)
+        else:
+           self.entry_message.bind('<Key-Return>', self.speak_contents)
+
+        def checkBinary():
+            try:
+                retcode = call("espeak")
+                if retcode < 0:
+                    print >>sys.stderr, "espeak was terminated by signal", -retcode
+                else:
+                    print >>sys.stderr, "espeak returned", retcode
+            except OSError as e:
+                print >>sys.stderr, "Execution failed:", e
+                return False
+            return True
+
+        if checkBinary():
+            print("proceed")
+        else:
+            print("find new file")
+            options = {}
+
+            options['title'] = "Locate eSpeak application"
+            options['parent'] = self.parent
+            options['initialdir'] = "C:\\Program Files (x86)\\"
+            options['filetypes'] = [('executables','.exe'),('all files','.*')]
+
+            retfilename = askopenfilename(**options)
+
+            if retfilename == "":
+                print("dialog cancelled")
+            else:
+                print("app location:", retfilename)
+                self.app.set(retfilename)
 
 
-
-
-    def speak_contents(self):
+    def speak_contents(self,*args):
         """
         Launches eSpeak.  Espeak must be in the path Environment.
         """
@@ -157,7 +209,16 @@ class MuteSpeak(Frame):
         args = self.espeaks_args()
 
         ## TODO: launch as a daemon thread so gui main thread remains responsive
-        os.system("espeak " + args + ' '+ '"' + repr(message) + '"')
+        # import os
+        # os.system("espeak " + args + ' '+ '"' + repr(message) + '"')
+        # todo: sanitize input or remove shell: https://docs.python.org/2/library/subprocess.html#frequently-used-arguments
+        appexec = self.app.get()
+
+        try:
+           call(self.app.get() + " " + args + ' '+ '"' + repr(message) + '"')
+        except OSError as e:
+            print >>sys.stderr, "execution failure: ", e
+
         self.entry_message.focus()
 
 
@@ -194,6 +255,10 @@ class MuteSpeak(Frame):
         except IOError:
             print('Could not save settings')
 
+
+    def report_callback_exception(self, *args):
+        err = traceback.format_exception(*args)
+        tkMessageBox.showerror('Exception', err)
 
 def load_defaults():
     """
